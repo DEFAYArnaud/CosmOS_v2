d1f0d1d3ff3691e4b3d03d749bc8a84d6ae3e37a
Ne0blast <romain.claveau@protonmail.ch>
2017-09-05 23:30:29 +0200

    MàJ interface [05.09.2017]
    - Vérification de la session avant affichage de l'interface

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<?php
+	session_start();
+
+	if(!isset($_SESSION['session']) && !empty($_SESSION['session'])) die("Accès refusé");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CosmOS</title>
+		<meta charset="utf-8" lang="fr/FR" />
+	</head>
+	
+	<body>
+	</body>
+</html>
