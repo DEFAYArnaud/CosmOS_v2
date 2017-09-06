32a2bbc1ee937285f27b7427c91042da35530955
Ne0blast <romain.claveau@protonmail.ch>
2017-09-06 16:08:13 +0200

    Design de base de l'interface [06.09.2017]
    - Design de base
    - Icônes de applications de base

--- app/index.php
+++ app/index.php
@@ -8,8 +8,35 @@
 	<head>
 		<title>CosmOS</title>
 		<meta charset="utf-8" lang="fr/FR" />
+		
+		<link rel="icon" href="public/images/icon.png" />
+		
+		<link rel="stylesheet" type="text/css" href="public/css/general.css" />
+		<link rel="stylesheet" type="text/css" href="public/css/header.css" />
+		<link rel="stylesheet" type="text/css" href="public/css/nav.css" />
+		
+		<script type="text/javascript" src="https://use.fontawesome.com/431c33e408.js"></script>
 	</head>
 	
 	<body>
+		<header>
+			<div id="menu">
+				<span><i class="fa fa-paint-brush"></i> Apparence</span>
+				<span><i class="fa fa-shopping-cart"></i> Boutique</span>
+				<span><i class="fa fa-server"></i> Stockage</span>
+				<span><i class="fa fa-user"></i> Mon compte</span>
+				<span><i class="fa fa-power-off"></i> Déconnexion</span>
+			</div>
+		</header>
+		
+		<nav>
+			<div><span><img src="applications/explorer/icon.png" /></span></div>
+			<div><span><img src="applications/organize/icon.png" /></span></div>
+			<div><span><img src="applications/document/icon.png" /></span></div>
+			<div><span><img src="applications/media/icon.png" /></span></div>
+			<div><span><img src="applications/code/icon.png" /></span></div>
+		</nav>
+		
+		<section></section>
 	</body>
 </html>
